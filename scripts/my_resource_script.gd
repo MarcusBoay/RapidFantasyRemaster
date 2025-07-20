95cac764c03a210457391876c0be7757db458cde
MarcusBoay <marcusboay@hotmail.com>
2025-07-19 22:02:05 -0400

    add tool to place player back to starting position

--- scripts/my_resource_script.gd
+++ scripts/my_resource_script.gd
@@ -1,11 +1,9 @@
 @tool
 extends Node2D
-class_name MyResourceScript
+class_name MyTools
 
-@export var hp: int:
-    get: return hp
-    set(x):
-        hp = clamp(x, 0, 255)
+@export var player: PlayerCharacter
+@export_tool_button("Place player back", "Callable") var place_player_back = place_player_back_func
 
 func _process(_delta):
     if Engine.is_editor_hint():
@@ -18,3 +16,11 @@ func _process(_delta):
         pass
 	# Code to execute both in editor and in game.
     pass
+
+func place_player_back_func():
+    if not player:
+        print("pls place player node in me c:")
+    else:
+        # starting position of player in the overworld
+        player.position.x = 984.0
+        player.position.y = 10080.0
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,18 @@
 extends Area2D
+class_name PlayerCharacter
 
-@onready var sprite = $Sprite2D
-@onready var up_raycast: RayCast2D = $UpRayCast2D
-@onready var down_raycast: RayCast2D = $DownRayCast2D
-@onready var left_raycast: RayCast2D = $LeftRayCast2D
-@onready var right_raycast: RayCast2D = $RightRayCast2D
-@onready var npc_tilemaplayer = %NPCs # TODO: idk if this should be a unique name...
+enum FACING_DIRECTION {
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT,
+}
+enum MOVEMENT_PHASE {
+	IDLE = 0,
+	ONE = 1,
+	TWO = 2,
+}
+
 
 var is_moving: bool = false
 var is_top_colliding = false
@@ -22,41 +29,12 @@ var right_texture = preload("res://assets/images_32x32/player_right.png")
 
 var dir_map: Dictionary[FACING_DIRECTION, DirectionStuff] = {}
 
-class DirectionStuff:
-	var name: String
-	var raycast: RayCast2D
-	var texture: Resource
-	var direction: FACING_DIRECTION:
-		get: return direction
-		set(dir):
-			direction = dir
-			if dir == FACING_DIRECTION.UP:
-				next_position += Vector2(0, -16)
-			elif dir == FACING_DIRECTION.DOWN:
-				next_position += Vector2(0, 16)
-			elif dir == FACING_DIRECTION.LEFT:
-				next_position += Vector2(-16, 0)
-			elif dir == FACING_DIRECTION.RIGHT:
-				next_position += Vector2(16, 0)
-	var next_position: Vector2
-
-	func _init(p_name: String, p_raycast: RayCast2D, p_texture: Resource, p_direction: FACING_DIRECTION) -> void:
-		self.name = p_name
-		self.raycast = p_raycast
-		self.texture = p_texture
-		self.direction = p_direction
-
-enum FACING_DIRECTION {
-	UP,
-	DOWN,
-	LEFT,
-	RIGHT,
-}
-enum MOVEMENT_PHASE {
-	IDLE = 0,
-	ONE = 1,
-	TWO = 2,
-}
+@onready var sprite = $Sprite2D
+@onready var up_raycast: RayCast2D = $UpRayCast2D
+@onready var down_raycast: RayCast2D = $DownRayCast2D
+@onready var left_raycast: RayCast2D = $LeftRayCast2D
+@onready var right_raycast: RayCast2D = $RightRayCast2D
+@onready var npc_tilemaplayer = %NPCs # TODO: idk if this should be a unique name...
 
 func _ready() -> void:
 	dir_map[FACING_DIRECTION.UP] = DirectionStuff.new("up", up_raycast, up_texture, FACING_DIRECTION.UP)
@@ -65,45 +43,46 @@ func _ready() -> void:
 	dir_map[FACING_DIRECTION.RIGHT] = DirectionStuff.new("right", right_raycast, right_texture, FACING_DIRECTION.RIGHT)
 
 func _physics_process(delta: float) -> void:
-	# process movement and facing direction
-	if movement_phase == MOVEMENT_PHASE.IDLE:
-		# face some direction
-		for dir in dir_map:
-			if Input.is_action_pressed(dir_map[dir].name):
-				facing = dir
-				sprite.texture = dir_map[dir].texture
+	if not Engine.is_editor_hint():
+		# process movement and facing direction
+		if movement_phase == MOVEMENT_PHASE.IDLE:
+			# face some direction
+			for dir in dir_map:
+				if Input.is_action_pressed(dir_map[dir].name):
+					facing = dir
+					sprite.texture = dir_map[dir].texture
 
-		next_pos = self.position
-		# attempt to move in some direction
-		for dir in dir_map:
-			if not is_colliding_with_boundary(dir_map[dir].raycast) and Input.is_action_pressed(dir_map[dir].name):
-				movement_phase = MOVEMENT_PHASE.ONE
-				break
+			next_pos = self.position
+			# attempt to move in some direction
+			for dir in dir_map:
+				if not is_colliding_with_boundary(dir_map[dir].raycast) and Input.is_action_pressed(dir_map[dir].name):
+					movement_phase = MOVEMENT_PHASE.ONE
+					break
 
-		# get next position because we are now moving
+			# get next position because we are now moving
+			if movement_phase != MOVEMENT_PHASE.IDLE:
+				get_next_pos()
+
+		# check if player is interacting
+		if movement_phase == MOVEMENT_PHASE.IDLE and Input.is_action_just_pressed("interact"):
+			for dir in dir_map:
+				if facing == dir_map[dir].direction:
+					if has_custom_data_layer(dir_map[dir].raycast, "Dialog"):
+						print_debug("TODO: pop open dialog menu")
+						# TODO: pop open dialog menu
+						pass
+					elif has_custom_data_layer(dir_map[dir].raycast, "MobID") and \
+						get_custom_data_layer(dir_map[dir].raycast, "MobID") != -1:
+						print_debug("TODO: pop open battle menu")
+						# TODO: pop open battle menu
+						pass
+
 		if movement_phase != MOVEMENT_PHASE.IDLE:
-			get_next_pos()
-
-	# check if player is interacting
-	if movement_phase == MOVEMENT_PHASE.IDLE and Input.is_action_just_pressed("interact"):
-		for dir in dir_map:
-			if facing == dir_map[dir].direction:
-				if has_custom_data_layer(dir_map[dir].raycast, "Dialog"):
-					print_debug("TODO: pop open dialog menu")
-					# TODO: pop open dialog menu
-					pass
-				elif has_custom_data_layer(dir_map[dir].raycast, "MobID") and \
-					 get_custom_data_layer(dir_map[dir].raycast, "MobID") != -1:
-					print_debug("TODO: pop open battle menu")
-					# TODO: pop open battle menu
-					pass
-
-	if movement_phase != MOVEMENT_PHASE.IDLE:
-		self.position = self.position.lerp(next_pos, delta+0.3)
-		if self.position.distance_to(next_pos) <= 1:
-			self.position = next_pos
-			movement_phase = (movement_phase + 1) % MOVEMENT_PHASE.size() as MOVEMENT_PHASE
-			get_next_pos()
+			self.position = self.position.lerp(next_pos, delta+0.3)
+			if self.position.distance_to(next_pos) <= 1:
+				self.position = next_pos
+				movement_phase = (movement_phase + 1) % MOVEMENT_PHASE.size() as MOVEMENT_PHASE
+				get_next_pos()
 
 func get_next_pos() -> void:
 	next_pos = self.position
@@ -139,3 +118,27 @@ func get_custom_data_layer(raycast: RayCast2D, layer_name: String) -> Variant:
 func get_tile_data(raycast: RayCast2D) -> TileData:
 	var coords = npc_tilemaplayer.get_coords_for_body_rid(raycast.get_collider_rid())
 	return npc_tilemaplayer.get_cell_tile_data(coords)
+
+class DirectionStuff:
+	var name: String
+	var raycast: RayCast2D
+	var texture: Resource
+	var direction: FACING_DIRECTION:
+		get: return direction
+		set(dir):
+			direction = dir
+			if dir == FACING_DIRECTION.UP:
+				next_position += Vector2(0, -16)
+			elif dir == FACING_DIRECTION.DOWN:
+				next_position += Vector2(0, 16)
+			elif dir == FACING_DIRECTION.LEFT:
+				next_position += Vector2(-16, 0)
+			elif dir == FACING_DIRECTION.RIGHT:
+				next_position += Vector2(16, 0)
+	var next_position: Vector2
+
+	func _init(p_name: String, p_raycast: RayCast2D, p_texture: Resource, p_direction: FACING_DIRECTION) -> void:
+		self.name = p_name
+		self.raycast = p_raycast
+		self.texture = p_texture
+		self.direction = p_direction
